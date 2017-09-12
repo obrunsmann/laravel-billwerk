49097460cd714dc9f24eb8e8ea5fd548022e797d
Oliver Brunsmann <brunsmann@lefamed.de>
2017-09-12 13:30:25 +0200

    Adds service provider
    Signed-off-by: Oliver Brunsmann <brunsmann@lefamed.de>

---
+++ src/LaravelBillwerkServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lefamed\LaravelBillwerk;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBillwerkServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
