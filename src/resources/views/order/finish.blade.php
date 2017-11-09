e2e354ddaa5b1a76e6aaec7045759a28b47d7eb6
Oliver Brunsmann <brunsmann@lefamed.de>
2017-11-09 21:17:04 +0100

    Updates finish view
    Signed-off-by: Oliver Brunsmann <brunsmann@lefamed.de>

--- src/resources/views/order/finish.blade.php
+++ src/resources/views/order/finish.blade.php
@@ -13,31 +13,8 @@
 @section('content')
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8">
-				<div id="order"></div>
-			</div>
-			<div class="col-md-4">
-				<div class="content-block">
-					<fieldset>
-						<legend>Rechnungsadresse</legend>
-
-
-						<address>
-							<strong>{{ $customer->customer_name }}</strong><br><br>
-
-							@if($customer->customer_sub_name)
-								{{ $customer->customer_sub_name }}<br>
-							@endif
-							{{ $customer->street }} {{ $customer->house_number }}<br>
-							{{ $customer->postal_code }} {{ $customer->city }}<br>
-							{{ $customer->country }}
-						</address>
-
-						<a href="{{ route('billwerk.order.changeAddress', ['planVariantId' => $planVariantId]) }}">
-							<i class="fa fa-pencil fa-fw"></i> Ändern
-						</a>
-					</fieldset>
-				</div>
+			<div class="col-sm-12">
+				<div id="order-finish"></div>
 			</div>
 		</div>
 	</div>
